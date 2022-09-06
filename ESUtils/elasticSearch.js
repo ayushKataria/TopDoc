05e85b5dabcccc75cd4ec02d750436a9b11f9602
Akash@22 <akash2202rits.com>
2022-09-06 23:37:28 +0530

    fetch user medical field

--- ESUtils/elasticSearch.js
+++ ESUtils/elasticSearch.js
@@ -4,7 +4,7 @@ const { json } = require('express');
 let elasticSearchClient=null
 //Akash Elastic pass
 
- var auth = 'elastic' + ":" + 'TNCn9w5B4rjmy*A+jJ0t'
+ var auth = 'elastic' + ":" + 'eu4oQfN7GXmxG0Z3nXvs'
 
  const connstring = "https://" + 'localhost' + ":" + '9200'
  const enable_password=true;
