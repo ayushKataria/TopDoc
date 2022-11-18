9571d775396f3372006929a171f95326427bb58f
Gagunsarthak <saisarthakmohanty1996@gmail.com>
2022-11-18 21:05:11 +0530

    cors enabled

--- index.js
+++ index.js
@@ -5,11 +5,15 @@ var compression = require("compression");
 //const bp = require('body-parser')
 const fileUpload = require("express-fileupload");
 // const esdb = require("./ESUtils/elasticSearch");
+const cors = require('cors');
+
 var app = express();
 app.use(express.json());
 app.use(morgan("tiny"));
 app.use(compression());
-
+app.use(cors({
+  origin: 'http://localhost:4200'
+}));
 app.use(
   fileUpload({
     useTempFiles: true,
