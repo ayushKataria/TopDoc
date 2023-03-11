68f48f23c9ba39bad8853ffce2cb652b5833ed17
Akash@22 <akash2202rits.com>
2023-03-11 17:09:56 +0530

    added mustache template

--- routes/notification/notificationType/mail.js
+++ routes/notification/notificationType/mail.js
@@ -1,4 +1,20 @@
 const nodemailer = require("nodemailer");
+const fs = require("fs");
+const mustache = require("mustache");
+// const templates = require("./constants/templates/forgetPassword.mustache");
+
+// const header = fs.readFileSync(templates, "utf8");
+const body = fs.readFileSync(
+  "constants/templates/forgetPassword.mustache",
+  "utf8"
+);
+const header = fs.readFileSync("constants/templates/header.mustache", "utf8");
+const footer = fs.readFileSync("constants/templates/footer.mustache", "utf8");
+
+// let name = "Akash";
+// const mailBody = mustache.render(body, { name });
+// console.log(mailBody, "mailbody");
+// const footer = fs.readFileSync(templates.footer.mustache, "utf8");
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
@@ -9,88 +25,31 @@ const transporter = nodemailer.createTransport({
 });
 
 async function userAnnouncementByMail(data, message) {
+  let name = "Akash";
+  const mailBody = mustache.render(header + body + footer, { name });
+  console.log(mailBody, "mailbody");
   console.log("inside userAnnouncementByMail", data);
   let options = {};
-  for (let i = 0; i < data.length; i++) {
-    options = {
-      from: "TopDoc_Official🩺 <topdoc.official22@gmail.com>",
-      to: [`${data[i].email}`],
-      subject: "Welcome to TopDoc",
-      html: `<!DOCTYPE html>
-        <html>
-        <head>
-          <title>Welcome to TopDoc 🩺</title>
-          <meta charset="UTF-8">
-          <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <style type="text/css">
-            body {
-              font-family: Arial, sans-serif;
-            }
-            .container {
-              width: 850px;
-              margin: 0 auto;
-            }
-            .header {
-              background-color: #F2F2F2;
-              padding: 20px;
-              text-align: center;
-            }
-            .header img {
-              width: 100%;
-                    height: 200px;
-                    
-            }
-            .content {
-              background-color: #FFFFFF;
-              padding: 20px;
-            }
-            .content h1 {
-              font-size: 36px;
-              margin-bottom: 20px;
-            }
-            .content p {
-              font-size: 18px;
-              line-height: 1.5;
-              margin-bottom: 20px;
-            }
-            .footer {
-              background-color: #F2F2F2;
-              padding: 20px;
-              text-align: center;
-              font-size: 14px;
-              color: #777777;
-            }
-          </style>
-        </head>
-        <body>
-          <div class="container">
-            <div class="header">
-              <img src="https://st3.depositphotos.com/2249091/13081/i/600/depositphotos_130811568-stock-photo-doctors-of-medicine-talking.jpg" alt="Company Logo">
-            </div>
-            <div class="content">
-              <h1 >Hello ${data[i].name},</h1>
-              <h1>Welcome to TopDoc 🩺!</h1>
-              <p>We are thrilled to have you as a part of our team. We are confident that you will make a valuable contribution to our organization and we look forward to your ideas and insights.</p>
-              <p>If you have any questions or need assistance with anything, please don't hesitate to reach out to our Whatsapp group.</p>
-              <p>Once again, welcome to the team!</p>
-            </div>
-            <div class="footer">
-              &copy; TopDoc , 2023
-            </div>
-          </div>
-           
-        </body>
-        </html>
-        `,
-    };
-    await transporter.sendMail(options, function (err, info) {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      console.log("sent:  " + info.response);
-    });
-  }
+  // for (let i = 0; i < data.length; i++) {
+  options = {
+    from: "TopDoc_Official🩺 <topdoc.official22@gmail.com>",
+    // to: [`${data[i].email}`],
+    to: [
+      "akashneekhara20@gmail.com",
+      "shukla.1997.sachin@gmail.com",
+      "saisarthakmohanty1996@gmail.com",
+    ],
+    subject: "Welcome to TopDoc",
+    html: mailBody,
+  };
+  await transporter.sendMail(options, function (err, info) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log("sent:  " + info.response);
+  });
+  // }
 }
-
+userAnnouncementByMail("data", "message");
 module.exports = { userAnnouncementByMail };
