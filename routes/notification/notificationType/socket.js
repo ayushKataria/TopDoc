a468ec664c5339b912b8f857a7696cc303bd6876
SachinShukla1997 <hello1997world1@gmail.com>
2023-04-05 21:51:06 +0530

    id added to socket

--- routes/notification/notificationType/socket.js
+++ routes/notification/notificationType/socket.js
@@ -14,34 +14,34 @@ io.on("connection", (socket) => {
 });
 function userAnnouncement(userIdList, body) {
   console.log("Send message called", userIdList);
-  try{
+  try {
+    let patient = [];
+    console.log("usersid", usersId);
+    let keyarr = usersId.map((obj) => Object.keys(obj)[0]);
+    console.log(keyarr, "keyarr", userIdList);
+    patient = userIdList.filter((element) =>
+      keyarr.includes(element.toString())
+    );
+    console.log("patient", patient);
 
- 
-  let patient = [];
-  console.log("usersid", usersId);
-  let keyarr = usersId.map((obj) => Object.keys(obj)[0]);
-  console.log(keyarr, "keyarr", userIdList);
-  patient = userIdList.filter((element) => keyarr.includes(element.toString()));
-  console.log("patient", patient);
-
-  if (body.tag.includes("QueueReload")) {
-    console.log("Queue reloaded")
-    patient.forEach((user) => {
-      io.to(usersId.map((obj) => obj[user])).emit("QueueReload", {
-        message: body.message,
+    if (body.tag.includes("QueueReload")) {
+      console.log("Queue reloaded");
+      patient.forEach((user) => {
+        io.to(usersId.map((obj) => obj[user])).emit("QueueReload", {
+          message: body.message,
+          id: user,
+        });
       });
-    });
-  } else {
-    patient.forEach((user) => {
-      io.to(usersId.map((obj) => obj[user])).emit("notification", {
-        message: body.message,
+    } else {
+      patient.forEach((user) => {
+        io.to(usersId.map((obj) => obj[user])).emit("notification", {
+          message: body.message,
+        });
       });
-    });
+    }
+  } catch (err) {
+    console.log("Error caught is ", err);
   }
-}
-catch(err){
-  console.log("Error caught is ",err)
-}
 }
 
 module.exports = {
